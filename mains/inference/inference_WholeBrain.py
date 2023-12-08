9ccaa0eeffe38c7089695242f43b00d093ab069c
wqlevi <37841818+wqlevi@users.noreply.github.com>
2023-12-08 12:30:31 +0100

    Update inference_WholeBrain.py

--- mains/inference/inference_WholeBrain.py
+++ mains/inference/inference_WholeBrain.py
@@ -28,27 +28,11 @@ def produce_whole(opt, nii_name, model):
     nb.save(new_nii, "%s/%s_sr_%s.nii.gz"%(save_name, file_name, str(opt['noise_level']).replace(".","")))
 
 def main(opt:dict):
-    #img = nb.load(opt['data_name'])
-    #img_arr = add_noise(img.get_fdata(), opt['noise_level']) # <= noise corruption
-    #img_ts = torch.Tensor(img_arr)[None,None,:,:,:]
-
-    #ckp_path =  opt['ckp_path']
-    #if opt['use_ln_model']:
-    #    #G = load_ln_G(ckp_path)
-    #    G = load_model(opt, state_key = 'state_dict',model_name=opt['model_name'],use_ln=True, device='cpu')
-    #else:
-    #    G = load_model(opt, state_key = 'Gnet_state_dict',device='cpu')
-    #y = G(img_ts)
-
-    #new_nii = nb.Nifti1Image(y.detach().squeeze().numpy(), img.affine)
-    #save_name = ckp_path.split("/")[-1].split(".")[0]
-    #file_name = img.get_filename().split("/")[-1].split(".")[0]
-    #nb.save(new_nii,'%s_%s_%s.nii.gz'%(save_name,file_name, str(opt['noise_level']).replace(".","")))
     G = load_ln_G(opt)
 
-    if opt['single_img']:
+    if opt['single_img']: # input a single image with complete directory
         produce_whole(opt, opt['data_path'], G)
-    else:
+    else:                 # input is a directory of the data's folder 
         for f in glob(opt['data_path']+"*.nii.gz"):
             produce_whole(opt, f, G)
 
